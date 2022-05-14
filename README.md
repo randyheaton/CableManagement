e6a90ea41782188074171759d90442bcd3cf44ae
randyheaton <randy.heaton@gmail.com>
2022-05-14 08:12:40 +0800

    simple proof of concept graph geometry

--- README.md
+++ README.md
@@ -4,38 +4,47 @@ a plugin description
 <!-- Plugin description end -->
 
 
-This is a plugin for Jetbrains IDEs. I started the project from a template provided by IntelliJ, and some
-of the naming conventions still reflect that. 
+This is a plugin for Android Studio. I started the project from a template provided by IntelliJ, and some
+of the names still probably reflect that. 
 
 
 
-<font size=5> Purpose:</font>
+#Purpose:
 
 Cable Management is an architectural pattern compliance assistant. While I was refactoring InjectorSuite to be more
 expressive of and adherent to its MVVM model, I realized that a visual aid would be pretty cool. Something that showed
 the model layers graphically and drew lines between dependencies and callers at each level. Sort of like really pretty cable
 management.
 
-<font size=5> How it works:</font>
+#How it works:
 
 Inside of a few text fields, you provide us with a list of the layers of your model, specifying their names and an integer representing their
-layer level in a hierarchy. Something like: "Views" are level 1, "Interface" is level 2, and "Disk" is level 3. 
+layer level in a hierarchy. Something like: "Views" are level 1, "Interface" is level 2, and "Serializer" is level 3. 
 
-We take that and make a JSON file to configure everything. From the JSON file, we write a Kotlin annotation class file into
+![Image](/readme_images/establish_model.png)
+
+
+We take that information and use it to write a JSON file and a Kotlin annotation class file into
 the resource folder of your project. 
+
+![Image](/readme_images/injected_files.png)
 
 From then on, you can mark your classes or functions, e.g.: @Views.
 
+![Images](/readme_images/annotation_use.png)
+
 Then when you're ready, we round up all of those annotated classes and make a neat graph showing the connections
-between actors at different levels. If it all works out correctly, you should see bipartite separation between each layer.
+between actors at different levels. If it all works out correctly, you should see nearly bipartite separation between each layer.
 Like tidy cable management. 
 
-<font size=5> Why it's cool:</font>
+![Images](/readme_images/generated_graph.png)
+
+#Why it's cool:
 
 The project is cool because we're using Compose for desktop inside of the IDE windows. That usage is not currently 
 supported but it is possible through Compose's ComposePanel(). 
 
-<font size=5> Where we're at:</font>
+#Where we're at:
 
 
 - Got the environment squared away. Lots of versioning issues with platform version (gradle properties in plugin build
@@ -48,10 +57,13 @@ needs correct version for receiving Android Studio instance) and JVM versions. T
 - Can generate dependency graph that is aware of simple function and constructor calls of type ``val x=Foo()``, 
 static invocations of type ``Foo.bar()``, and factory invocations of type ``val x=Builder<Foo>()``.
 
-<font size=5>What's next:</font>
+#What's next:
 
 Lots of hardcoding of folder names relying on assumptions about project structure need to be fixed.
 
-And then we have to figure out a good way to generate the graphic. My instinct says a Canvas because leaning on 
-ready-made composables doesn't seem feasible because of the geometry. If the IDE windows will play well with a 
-webview, we could dispatch graphics to js. I don't know yet. 
+There are probably a lot of dependencies that don't come in the form of simple calls ``val x=Foo()``,
+static invocations ``Foo.bar()``, or factory invocations ``val x=Builder<Foo>()``
+
+Also, the geometry isn't scalable. It's all hardcoded for the demo you see in the screenshots. 
+
+
--- src/main/kotlin/com/injectorsuite/cablemanagement/actions/ShowModelAdherence.kt
+++ src/main/kotlin/com/injectorsuite/cablemanagement/actions/ShowModelAdherence.kt
@@ -1,47 +1,43 @@
 package com.injectorsuite.cablemanagement.actions
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.awt.ComposePanel
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Fill
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.unit.dp
-import com.intellij.ide.fileTemplates.FileTemplateManager
-import com.intellij.ide.fileTemplates.FileTemplateUtil
-import com.intellij.lang.ASTNode
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.psi.PsiElement
-import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.roots.ContentIterator
 import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.ui.DialogWrapper
-import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiManager
 import com.intellij.psi.search.FilenameIndex
 import com.intellij.psi.search.GlobalSearchScope
-import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.json.Json
-import org.jetbrains.kotlin.idea.caches.resolve.KtFileClassProviderImpl
 import org.jetbrains.kotlin.idea.debugger.sequence.psi.callName
-import org.jetbrains.kotlin.idea.refactoring.move.moveFilesOrDirectories.KotlinMoveDirectoryWithClassesHelper
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.psiUtil.collectDescendantsOfType
 import org.jetbrains.kotlin.psi.psiUtil.getChildrenOfType
-import org.jetbrains.kotlin.resolve.calls.callUtil.getResolvedCall
+import org.jetbrains.skia.Font
+import org.jetbrains.skia.Paint
+import org.jetbrains.skia.TextLine
 //import org.jetbrains.kotlin.psi.*
-import java.io.FileInputStream
 import javax.swing.JComponent
 
 
 
 
-class Test: AnAction() {
+class ShowModelAdherence: AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         val psiManager=PsiManager.getInstance(e.project!!)
         val ktp= KTProcessor(psiManager)
@@ -56,11 +52,7 @@ class Test: AnAction() {
             graph.add(newNode)
         }
 
-        val text=graph.fold("",{acc,next->
-            acc+next.toString()+"\n"
-        })
-
-        ActionWrapper(text).show()
+        GraphDialogWrapper(graph,ktp.model).show()
     }
 }
 
@@ -108,30 +100,40 @@ data class Layer(val name:String,val level:Int)
 @kotlinx.serialization.Serializable
 data class Model(val layers:List<Layer>)
 
-class NaiveGraphBuilder(){
 
-    fun buildGraph(text:String,model: Model):String{
-        return model.layers.map{it->it.level}.toSortedSet().fold(""){acc,next->
-            acc+"Level "+next.toString()+": "+model.layers.filter{it.level==next}.fold(""){line,layer->
-                line+layer.name+", "
-            }.removeSuffix(", ")+"\n"
-        }
-    }
-
-}
-
-
-class ActionWrapper(val text:String): DialogWrapper(true) {
+class GraphDialogWrapper(val graph:MutableList<Node>,val model:Model): DialogWrapper(true) {
+    val positionMap=mutableMapOf<Node,Offset>()
     init{
         init()
-        title="a demo"
+        title="Model Adherence"
         setSize(500,500)
     }
     override fun createCenterPanel(): JComponent? {
         return ComposePanel().apply {
             setContent {
                 Column(Modifier.size(500.dp)) {
-                    Text(text)
+                    Canvas(Modifier){
+                        model.layers.sortedBy{it.level}.forEachIndexed{outerIndex,layer->
+                            val rectSize=graph.count{node->node.layer==layer.name}
+                            drawRect(Color.Black, Offset(10f,150f*outerIndex+10f), Size(rectSize*200f,50f),1f,Stroke(3f))
+                            drawContext.canvas.nativeCanvas.apply{
+                                drawTextLine(TextLine.make(layer.name+" Layer",Font()),rectSize*100f,150f*outerIndex+75f,Paint())
+                            }
+                            graph.filter{node->node.layer==layer.name}.forEachIndexed{innerIndex,node->
+                                val center=Offset(30f+200f*innerIndex,150f*outerIndex+35f)
+                                drawCircle(Color.Blue,10f,center,1f,Fill)
+                                drawContext.canvas.nativeCanvas.apply{
+                                    drawTextLine(TextLine.make(node.name, Font()),center.x+15f,center.y,Paint())
+                                }
+                                positionMap.put(node,center)
+                            }
+                        }
+                        graph.forEach{node->
+                            graph.filter{other->other.dependents.contains(node.name)}.forEach{other->
+                                drawLine(Color.Green,positionMap[other]!!,positionMap[node]!!)
+                            }
+                        }
+                    }
 
                 }
             }
